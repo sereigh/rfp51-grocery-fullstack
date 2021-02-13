26dd9cb1b20c83392b7435bb2cc060104f361540
Krista <kb9286@gmail.com>
2021-02-13 14:21:33 -0600

    server now serves /src and app renders to page

--- server/index.js
+++ server/index.js
@@ -4,15 +4,14 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require("cors");
-const db = require("../db/index.js");
 const routes = require("./routes.js");
 
 app.use(cors());
 app.use('/', routes);
-
+app.use(express.static('src'));
 
 app.listen(port, () => {
-  console.log("listening on 8080");
+  console.log(`listening on localhost:${port}`);
 });
 
 
