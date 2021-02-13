cf893c5f1fd8fe3ff576b3c291e6ddd7731b2e78
Krista <kb9286@gmail.com>
2021-02-13 16:12:32 -0600

    refactored app to class component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import GroceryList from './GroceryList.jsx';
 
-const App = () => (
-  <h1>'HEYEYEYEYEYE'</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //state
+    //bind
+  }
+  //handlers
+  //lifecycle
+  render() {
+    return (
+      <h1>'HEYEYEYEYEYE'</h1>
+    );
+  }
+};
 
 export default App;
 
